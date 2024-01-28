f7db2a4ac57a375a93f79c31b758b875d88b1e5c
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-01-28 04:35:07 +0300

    Shimmer and card Layouts OK

--- app/src/main/java/com/example/movieapp/presentation/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/fragments/home/HomeFragment.kt
@@ -5,17 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_home,container,false)
+
+        showShimmer()
+
+        return binding.root
     }
 
+
+
+    private fun showShimmer(){
+        binding.shimmerFrameLayout.visibility = View.VISIBLE
+        binding.rVPopular.visibility = View.INVISIBLE
+        binding.rVComingSoon.visibility = View.INVISIBLE
+        binding.rVTrendingMovies.visibility = View.INVISIBLE
+        binding.rVTrendingTV.visibility = View.INVISIBLE
+
+        binding.textViewPopular.visibility = View.INVISIBLE
+        binding.textViewComingSoon.visibility = View.INVISIBLE
+        binding.textViewTrendingMovie.visibility = View.INVISIBLE
+        binding.textViewTrendingTv.visibility = View.INVISIBLE
+
+        binding.shimmerFrameLayout.showShimmer(true)
+    }
+//    private fun hideShimmer(){
+//
+//        binding.shimmerFrameLayout.visibility = View.INVISIBLE
+//        binding.rVPopular.visibility = View.VISIBLE
+//        binding.rVComingSoon.visibility = View.VISIBLE
+//        binding.rVTrendingMovies.visibility = View.VISIBLE
+//        binding.rVTrendingTV.visibility = View.VISIBLE
+//        binding.shimmerFrameLayout.hideShimmer()
+//
+//
+//    }
 }
---
+++ app/src/main/java/com/example/movieapp/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.util
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.themoviedb.org"
+        const val API_KEY = "7c646d0a3ce431c66c2408250dbc2bfe"
+
+    }
+
+}
