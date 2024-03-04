e7234634b4fd5ebdbd8a4ed7e03b64b84e771d82
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-03-04 17:19:12 +0300

    fragment_details.xml Designed

--- app/src/main/java/com/example/movieapp/presentation/ui/fragments/details/DetailsFragment.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/fragments/details/DetailsFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentDetailsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_details,container,false)
+
+
+
+        return binding.root
     }
 
 }
