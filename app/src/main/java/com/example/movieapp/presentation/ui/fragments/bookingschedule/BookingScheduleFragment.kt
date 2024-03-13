dd790c172a4a2bf98035ca472023af97d1f66c44
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-03-13 16:41:14 +0300

    BookingScheduleFragment OK

--- app/src/main/java/com/example/movieapp/presentation/ui/fragments/bookingschedule/BookingScheduleFragment.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/fragments/bookingschedule/BookingScheduleFragment.kt
@@ -15,48 +15,94 @@ import androidx.navigation.fragment.navArgs
 import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentBookingScheduleBinding
 import com.example.movieapp.presentation.ui.viewmodels.BookingViewModel
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.text.SimpleDateFormat
 import java.util.Locale
 
 class BookingScheduleFragment : Fragment() {
 
     private lateinit var binding: FragmentBookingScheduleBinding
 
-    private lateinit var dateAdapter:ArrayAdapter<String>
+    private lateinit var dateAdapter: ArrayAdapter<String>
     private lateinit var viewModel: BookingViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_booking_schedule,container,false)
+        binding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.fragment_booking_schedule,
+            container,
+            false
+        )
 
 
         viewModel.loadItems()
 
-        val bundle : BookingScheduleFragmentArgs by navArgs()
+        val bundle: BookingScheduleFragmentArgs by navArgs()
         binding.detailsResult = bundle.detailsResult
-        binding.vote =  String.format(Locale.US,"%,.1f",bundle.detailsResult.voteAverage)
+        binding.vote = String.format(Locale.US, "%,.1f", bundle.detailsResult.voteAverage)
 
+
+
+
+        binding.apply {
+            dateTimeStarDustChipGroup. setOnCheckedStateChangeListener { group, checkedId ->
+                clearOtherChipGroupSelection(group, binding.dateTimeCosmosChipGroup)
+            }
+
+            dateTimeCosmosChipGroup. setOnCheckedStateChangeListener { group, checkedId ->
+                clearOtherChipGroupSelection(group, binding.dateTimeStarDustChipGroup)
+            }
+        }
+
+
+        //Read Spinner
+
+        viewModel.selectedDay.observe(viewLifecycleOwner) {
+            binding.dateTimeStarDustChipGroup.clearCheck()
+            binding.dateTimeCosmosChipGroup.clearCheck()
+            viewModel.disableChipsBasedOnTime(binding.dateTimeStarDustChipGroup, it)
+            viewModel.disableChipsBasedOnTime(binding.dateTimeCosmosChipGroup, it)
+        }
 
 
         // spinner item observe
         viewModel.items.observe(viewLifecycleOwner) { items ->
             items?.let {
-                Log.e("Dante","frg")
-                dateAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, it)
+                Log.e("Dante", "frg")
+                dateAdapter =
+                    ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, it)
                 dateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 binding.spinner.adapter = dateAdapter
             }
         }
 
         binding.button2.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_bookingScheduleFragment_to_bookNowFragment)
+            val action =
+                BookingScheduleFragmentDirections.actionBookingScheduleFragmentToBookNowFragment(
+                    bundle.detailsResult
+                )
+            Navigation.findNavController(it).navigate(action)
         }
 
 
 
         binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 viewModel.onItemSelected(position)
+                val selectedDateString = parent?.getItemAtPosition(position).toString()
+                val selectedDate =
+                    SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse(selectedDateString)
+
+                // ViewModel içindeki LiveData'ye seçilen tarihi ata
+                viewModel.setSelectedDate(selectedDate!!)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -74,6 +120,19 @@ class BookingScheduleFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(BookingViewModel::class.java)
     }
 
+
+    private fun clearOtherChipGroupSelection(
+        selectedChipGroup: ChipGroup,
+        otherChipGroup: ChipGroup
+    ) {
+        for (i in 0 until selectedChipGroup.childCount) {
+            val chip: Chip = selectedChipGroup.getChildAt(i) as Chip
+            if (chip.isChecked) {
+                otherChipGroup.clearCheck()
+                return
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/movieapp/presentation/ui/viewmodels/BookingViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/viewmodels/BookingViewModel.kt
@@ -1,17 +1,30 @@
 package com.example.movieapp.presentation.ui.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
-class BookingViewModel @Inject constructor():ViewModel() {
+class BookingViewModel @Inject constructor() : ViewModel() {
 
+    private val _selectedDate = MutableLiveData<Date>()
+    val selectedDay: LiveData<Date>
+        get() = _selectedDate
+
+    fun setSelectedDate(date: Date) {
+        _selectedDate.value = date
+    }
+
+    /////////////////////////////
     private val _selectedItem = MutableLiveData<String>()
     val selectedItem: LiveData<String>
         get() = _selectedItem
@@ -24,24 +37,52 @@ class BookingViewModel @Inject constructor():ViewModel() {
         loadItems()
     }
 
-     fun loadItems() {
-         val dateArray = ArrayList<String>()
-         val calendar = Calendar.getInstance()
+    fun loadItems() {
+        val dateArray = ArrayList<String>()
+        val calendar = Calendar.getInstance()
 
-         val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-         dateArray.add(dateFormat.format(calendar.time)) // Bugünün tarihini ekle
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        dateArray.add(dateFormat.format(calendar.time)) // Bugünün tarihini ekle
 
-         repeat(5) {
-             calendar.add(Calendar.DATE, 1) // Sonraki günü ekle
-             dateArray.add(dateFormat.format(calendar.time))
-         }
+        repeat(5) {
+            calendar.add(Calendar.DATE, 1) // Sonraki günü ekle
+            dateArray.add(dateFormat.format(calendar.time))
+        }
 
-         _items.value = dateArray
+        _items.value = dateArray
     }
 
     fun onItemSelected(position: Int) {
         if (position > 0) {
             _selectedItem.value = _items.value?.get(position)
+        }
+    }
+
+    fun disableChipsBasedOnTime(chipGroup: ChipGroup, readDate: Date) {
+        val calendar = Calendar.getInstance()
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        for (i in 0 until chipGroup.childCount) {
+            val chip = chipGroup.getChildAt(i) as Chip
+
+            // Chip'in textinden saat bilgisini al
+            val hour = chip.text.toString().substringBefore(":").toInt()
+            Log.e("Dante", "currentHour: $currentHour")
+
+            val chipDate = Calendar.getInstance().apply {
+                time = readDate
+            }.get(Calendar.DAY_OF_MONTH)
+
+            if (currentDay == chipDate && hour <= currentHour) {
+                chip.isEnabled = false
+
+
+            } else {
+                chip.isEnabled = true
+
+
+            }
         }
     }
 }
