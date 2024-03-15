1d536c31ea3a33e77d418bbeb8db511116d6db01
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-03-15 20:37:26 +0300

    FirebaseOperationsViewModel and Booking Class Added

---
+++ app/src/main/java/com/example/movieapp/presentation/ui/viewmodels/FirebaseOperationsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.presentation.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FirebaseOperationsViewModel @Inject constructor(): ViewModel() {
+}
