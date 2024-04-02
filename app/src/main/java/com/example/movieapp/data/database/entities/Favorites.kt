bbc42b057a051dde64766ba5172b185f3f0ff9fd
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-04-02 14:00:47 +0300

    Favroites data class adjusted to "Room" requirements

--- app/src/main/java/com/example/movieapp/data/database/entities/Favorites.kt
+++ app/src/main/java/com/example/movieapp/data/database/entities/Favorites.kt
@@ -1,9 +1,16 @@
 package com.example.movieapp.data.database.entities
 
-class Favorites(
-    val movieId: Int,
-    val movieName: String,
-    val moviePosterPath: String,
-    val moviewOverview: String,
-    val rating: Double
-    )
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "favorites")
+data class Favorites(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "favMovieId") val favMovieId: Int,
+    @ColumnInfo(name = "movieId") @NotNull var movieId: Int,
+    @ColumnInfo(name = "movieName") @NotNull val movieName: String,
+    @ColumnInfo(name = "moviePosterPath") @NotNull val moviePosterPath: String,
+    @ColumnInfo(name = "movieOverview") @NotNull val movieOverview: String,
+    @ColumnInfo(name = "movieRating") @NotNull val rating: Double
+)
